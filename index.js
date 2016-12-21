8a0693cb080ddfcdc886636502c5e9559daaf763
荣乾 <lygjcsrq@163.com>
2016-12-21 15:54:52 +0800

    add test

--- index.js
+++ index.js
@@ -9,22 +9,38 @@ function Offset(x, y) {
 
 Offset.isOffset = function(offset) {
   return offset instanceof Offset;
-}
+};
 
 Offset.prototype.x = function() {
   return this._x;
-}
+};
 
 Offset.prototype.y = function() {
   return this._y;
-}
+};
 
 Offset.prototype.distance = function() {
   return Math.sqrt(this.distanceSqr());
-}
+};
 
 Offset.prototype.distanceSqr = function() {
   return this.x()*this.x()+this.y()*this.y();
+};
+
+Offset.prototype.reverse = function() {
+  return Offset(-this.x(), -this.y());
+};
+
+Offset.prototype.clone = function() {
+  return Offset(this.x(), this.y());
+};
+
+Offset.equals = function(offset1, offset2) {
+  return offset1.x() == offset2.x() && offset1.y() == offset2.y();
+}
+
+Offset.prototype.equals = function(another) {
+  return Offset.equals(this, another);
 };
 
 Offset.moveup = function(y) {
--- test/test.js
+++ test/test.js
@@ -1,2 +1,62 @@
-var uiOffset = require('..');
+var Offset = require('..');
 var expect = require('expect');
+
+describe('Offset', function(){
+  it('should work', function() {
+    var offset = Offset(3, 4);
+    expect(offset.x()).toBe(3);
+    expect(offset.y()).toBe(4);
+    expect(Offset.isOffset(offset)).toBe(true);
+  });
+});
+
+describe('Offset#distance', function() {
+  it('should work', function() {
+    var offset = Offset(3, 4);
+    expect(offset.distance()).toBe(5);
+  });
+});
+
+describe('Offset#reverse', function() {
+  it('should work', function(){
+    var offset = Offset(3, 4);
+    offset = offset.reverse();
+    expect(offset.x()).toBe(-3);
+    expect(offset.y()).toBe(-4);
+  });
+  it('should not effect itself', function(){
+    var offset = Offset(3, 4);
+    offset.reverse();
+    expect(offset.x()).toBe(3);
+    expect(offset.y()).toBe(4);
+  });
+  it('should clone new one', function(){
+    var offset = Offset(3, 4);
+    expect(offset.reverse()).toNotBe(offset);
+  });
+});
+
+describe('Offset#clone', function(){
+  it('should work', function(){
+    var offset = Offset(3, 4);
+    var another = offset.clone();
+    expect(offset.x()).toBe(another.x());
+    expect(offset.y()).toBe(another.y());
+    expect(offset).toNotBe(another);
+  });
+});
+
+describe('Offset#equals', function() {
+  it('should work', function(){
+    var one = Offset(3 ,4);
+    var other = Offset(3, 4);
+    expect(one.equals(other)).toBe(true);
+    expect(other.equals(one)).toBe(true);
+  });
+  it('should tell not equals', function() {
+    var one = Offset(3 ,4);
+    var other = Offset(4, 3);
+    expect(one.equals(other)).toBe(false);
+    expect(other.equals(one)).toBe(false);
+  });
+});
